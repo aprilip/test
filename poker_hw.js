b0ee01f1102ecfa8823bbe7788453484c3f3e5fd
aprilip <79826761+aprilip@users.noreply.github.com>
2021-10-19 22:48:52 +0800

    20211019

--- poker_hw.js
+++ poker_hw.js
@@ -4,10 +4,10 @@ for(var i=0;i<52;i++)
 switch(parseInt(i/13))
 {
     case 0:
-        if (parseInt(i%13)<9){
+        if (parseInt(i%13)<9){//因比數字大小的話，H2會比H12大（由第一個數字判斷），故需將1~9改為字串01~09
             poker.push("H"+"0"+(parseInt(i%13)+1));
         }
-        else{
+        else{//10~13則不符上述故跳出直接印出
         poker.push("H"+(parseInt(i%13)+1));}
         break;
     case 1:
@@ -60,22 +60,22 @@ poker.splice(rand,1);
 }
 function compare(a,b)
 {
- if(b.charCodeAt(0)==a.charCodeAt(0))//如果第一個字相同
+ if(b.charCodeAt(0)==a.charCodeAt(0))//如果第一個字相同（英文）大小寫ex:A,a代碼不同
 {
   // compare charAt(1),charAt(2)
 
-  if(b.charCodeAt(1)==a.charCodeAt(1))//如果第二個字相同
+  if(b.charAt(1)==a.charAt(1))//如果第二個字相同（數字）沒有大小寫故代碼一樣
  {
-  return b.charAt(2)-a.charAt(2)//比第三個字
+  return b.charAt(2)-a.charAt(2)//比第三個字（數字）
  }
  else
  {
-  return b.charAt(1)-a.charAt(1)//比第二個字
+  return b.charAt(1)-a.charAt(1)//比第二個字（數字）
  }
 }
  else
 {
- return b.charCodeAt(0)-a.charCodeAt(0)//比第一個字
+ return b.charCodeAt(0)-a.charCodeAt(0)//比第一個字（英文）
 }
 }
 
