ed3a05d3c3582f5e3367fb29967a44088af0f139
aprilip <79826761+aprilip@users.noreply.github.com>
2021-12-08 12:31:36 +0800

    20211208hw

--- life2.js
+++ life2.js
@@ -1,17 +1,17 @@
+const myobject = document.getElementById("gameBorad");
+const mywidth = myobject.offsetWidth;  //寬度
+//const myheight = myobject.offsetHeight;  //高度
+//const mywidth=document.body.clientWidth;
 const LIVE=1, DEAD=0; 
-const mywidth=600;
 class Life {
-    constructor(_row,_col)
-    {
+    constructor(_row,_col){
         this.row = _row;
         this.col = _col;
         this.grid = new Array();
         //create 2d array
-        for (let r = 0; r < this.row; r++) 
-        {
+        for (let r = 0; r < this.row; r++) {
            this.grid.push(new Array());
-           for (let c = 0; c < this.col; c++) 
-           {
+           for (let c = 0; c < this.col; c++) {
                this.grid[r].push(DEAD);
            }
         }
@@ -20,23 +20,20 @@ class Life {
     // Initinalize(){
     // }
 };
-Life.prototype.Initialize = function()
-{
+Life.prototype.Initialize = function(){
     this.grid[3][4] = LIVE;
     this.grid[3][5] = LIVE;
     this.grid[3][6] = LIVE;
     //this.grid[3][7] = LIVE;
 }
-Life.prototype.getStatusAt = function(row, col)
-{
+Life.prototype.getStatusAt = function(row, col){
     if(row<0 || col <0)
        return DEAD;
     if(row >= this.row || col >= this.col)  
        return DEAD;
     return this.grid[row][col];
 }
-Life.prototype.setStatusAt = function(row, col, status)
-{
+Life.prototype.setStatusAt = function(row, col, status){
     if(row<0 || col <0)
        return false;
     if(row >= this.row || col >= this.col)  
@@ -44,8 +41,7 @@ Life.prototype.setStatusAt = function(row, col, status)
     this.grid[row][col]=status;
     return true;
 }
-Life.prototype.neighborCount = function(row, col)
-{
+Life.prototype.neighborCount = function(row, col){
     var count=0;
     count += this.getStatusAt(row-1, col-1);
     count += this.getStatusAt(row-1, col);
@@ -67,10 +63,8 @@ Life.prototype.update = function(){
     // }
     // this.grid; //no duplicate /copy
     var nextGrid = JSON.parse(JSON.stringify(this.grid));
-    for (let r = 0; r < this.row; r++) 
-    {
-        for (let c = 0; c < this.col; c++) 
-        {
+    for (let r = 0; r < this.row; r++) {
+        for (let c = 0; c < this.col; c++) {
             var nCount = this.neighborCount(r, c);
             if(nCount == 3 && this.getStatusAt(r,c)==DEAD) //DEAD => LIVE
                nextGrid[r][c] = LIVE;
@@ -91,13 +85,10 @@ class Board{
 
      draw(){
         this.ctx2d.clearRect(0,0,mywidth,mywidth);
-       for (let r = 0; r < this.game.row; r++) 
-       {
-           for (let c = 0; c < this.game.col; c++) 
-           {
+       for (let r = 0; r < this.game.row; r++) {
+           for (let c = 0; c < this.game.col; c++) {
                
-               if(this.game.getStatusAt(r,c)==LIVE)
-               {
+               if(this.game.getStatusAt(r,c)==LIVE){
                    		//fill
                    this.ctx2d.fillRect(c*this.size, r*this.size, this.size, this.size);
             			//        this.ctx2d.strokeRect(c*this.size, r*this.size, this.size, this.size);
@@ -111,18 +102,16 @@ class Board{
        } 
     }
 
-DrawPoint(r,c)
-{ //只重畫點過的位置canvas
-	if(this.game.getStatusAt(r,c)==LIVE)//取得（給予）狀態
-    {
-          		this.ctx2d.fillStyle = "#ff0000";//紅色                	 
-            		    
-            }else
-            {
-			this.ctx2d.fillStyle = "#ffffff";//白色	
+DrawPoint(r,c){ //只重畫點過的位置canvas
+	if(this.game.getStatusAt(r,c)==LIVE){
+          		this.ctx2d.fillStyle = "#ff0000";                	 
+            		    }else{
+			this.ctx2d.fillStyle = "#ffffff";	
           }
-		this.ctx2d.fillRect(c*this.size, r*this.size, this.size, this.size);//上色色塊大小
-		this.ctx2d.fillStyle = "#ff0000";//上色	
+		//this.ctx2d.fillRect(c*this.size+1, r*this.size+1, this.size-1, this.size-1);
+		this.ctx2d.fillRect(c*this.size, r*this.size, this.size, this.size);
+		this.ctx2d.fillStyle = "#ff0000";
+		this.ctx2d.strokeRect(c*this.size, r*this.size, this.size, this.size);
    }
 }
 
@@ -145,11 +134,9 @@ function next(){
  {
      var col = Math.floor(event.offsetX/gameBorad.size);
      var row = Math.floor(event.offsetY/gameBorad.size);
-     if(game.getStatusAt(row, col)== LIVE)
-     {
+     if(game.getStatusAt(row, col)== LIVE){
         game.setStatusAt(row,col, DEAD);
-     }else
-    {
+     }else{
          game.setStatusAt(row,col, LIVE);
 	}
         //gameBorad.draw();
